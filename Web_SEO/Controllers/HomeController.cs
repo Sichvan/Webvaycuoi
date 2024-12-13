7056d1eaa062855792bde556307b309ad3095228
Sichvan <vanghoangky2511@gmail.com>
2024-12-13 11:14:41 +0700

    tieuluanSEO

---
+++ Web_SEO/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_SEO.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Trang chủ váy cưới
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Shop hay Váy cưới
+        public ActionResult Shop()
+        {
+            return View();
+        }
+
+        //Testimonial hay Chứng nhận, nhận xét 
+        public ActionResult Testimonial()
+        {
+            return View();
+        }
+
+        //Contact hay liên hệ  
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+      
+    }
+}
